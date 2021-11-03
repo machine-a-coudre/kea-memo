a2895e4e56537cbad82acba29efab7e8ec481475
Alita <dev@pinolatine.com>
2021-11-03 15:11:31 +0100

    Add reset CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component} from "react";
 import { hot } from "react-hot-loader";
 import Game from "./component/Game";
+import "normalize.css"
 import "./App.css";
 
 class App extends Component{
