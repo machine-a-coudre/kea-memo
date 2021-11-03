780b8b95ded36e1d36e8adfdd113d75e4f213074
Alita <dev@pinolatine.com>
2021-11-03 14:30:27 +0100

    Init empty project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/env"]
+                }
+            }, {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx"]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
+};
