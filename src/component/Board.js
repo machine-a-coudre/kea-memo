d39d6bfc6c2b2b5053b9fa860909e4b9d228916f
Alita <dev@pinolatine.com>
2021-11-06 15:55:57 +0100

    Handle status message start/click card/found or failed

--- src/component/Board.js
+++ src/component/Board.js
@@ -10,9 +10,10 @@ export default class Board extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { cards: [], distinctValue: [] };        
+        this.state = { cards: [], distinctValue: [], gameStarted: false, gameMessage: "Start the game: click on a card." };
         this.reversedCard = [];
         this.foundCard = [];
+        this.newGame = true;
     }
     
     /**
@@ -39,19 +40,27 @@ export default class Board extends React.Component {
                 if (reversedCard.length === 2) {
                     // Is same value ? validate
                     if (reversedCard[ 0 ].props.value === reversedCard[ 1 ].props.value) {
+                        board.setState({ gameMessage: "All right!" });
+
                         board.foundCard = [ reversedCard[ 1 ], reversedCard[ 0 ], ... board.foundCard ];
 
                         // Reset reversedCards
-                        if (board.foundCard.length < board.cards.length) {
+                        if (board.foundCard.length < board.state.cards.length) {
                             board.reversedCard = [];
+
+                            setTimeout(() => { board.setState({ gameMessage: "Click on a card." }) }, 1000);
                         }
                     } else {
+                        board.setState({ gameMessage: "Oh no! Try again..." });
+
                         // Not same value, revert cards
                         setTimeout(() => {
                             reversedCard.map((_card) => { _card.toggleStateReverse() });
 
                             // Reset reversedCards
                             board.reversedCard = [];
+
+                            board.setState({ gameMessage: "Click on a card." });
                         }, 1000);
                     }
                 }
@@ -60,6 +69,8 @@ export default class Board extends React.Component {
             } else {
                 reject(board);
             }
+
+            board.setState({ gameStarted: true });
         });
     }
     
@@ -75,56 +86,61 @@ export default class Board extends React.Component {
     render() {
         const className = ["component-board"];
         const board = this;
-        let cards = [], distinctValue = [];
+        const cards = this.state.cards;
+        const distinctValue = this.state.distinctValue;
 
-        function getRandomArbitrary(min, max) {
-            return Math.floor(Math.random() * (max - min)) + min;
-        }
-
-        // https://stackoverflow.com/a/2450976
-        function shuffle(array) {
-            let currentIndex = array.length,  randomIndex;
-            
-            // While there remain elements to shuffle...
-            while (currentIndex != 0) {
-                // Pick a remaining element...
-                randomIndex = Math.floor(Math.random() * currentIndex);
-                currentIndex--;
-            
-                // And swap it with the current element.
-                [array[currentIndex], array[randomIndex]] = [
-                array[randomIndex], array[currentIndex]];
+        if (this.newGame === true) {
+            function getRandomArbitrary(min, max) {
+                return Math.floor(Math.random() * (max - min)) + min;
             }
-            
-            return array;
-        }
 
-        let n = 0;
-
-        while (n < board.props.difficultyLvl) {
-            let random = getRandomArbitrary(0, 100);
-
-            if (distinctValue.indexOf(random) === -1) {
-                distinctValue.push(random);
-                n++
+            // https://stackoverflow.com/a/2450976
+            function shuffle(array) {
+                let currentIndex = array.length,  randomIndex;
+                
+                // While there remain elements to shuffle...
+                while (currentIndex != 0) {
+                    // Pick a remaining element...
+                    randomIndex = Math.floor(Math.random() * currentIndex);
+                    currentIndex--;
+                
+                    // And swap it with the current element.
+                    [array[currentIndex], array[randomIndex]] = [
+                    array[randomIndex], array[currentIndex]];
+                }
+                
+                return array;
             }
+
+            let n = 0;
+
+            while (n < board.props.difficultyLvl) {
+                let random = getRandomArbitrary(0, 100);
+
+                if (distinctValue.indexOf(random) === -1) {
+                    distinctValue.push(random);
+                    n++
+                }
+            }
+
+            distinctValue.map((val) => {
+                cards.push(board.renderCards(`card-1${val}`, val, React.createRef()));
+                cards.push(board.renderCards(`card-2${val}`, val, React.createRef()));
+            });
+
+            // Shuffle card for random display
+            shuffle(cards);
+
+            this.newGame = false;
         }
-
-        distinctValue.map((val) => {
-            cards.push(board.renderCards(`card-1${val}`, val, React.createRef()));
-            cards.push(board.renderCards(`card-2${val}`, val, React.createRef()));
-        });
-
-        // Shuffle card for random display
-        shuffle(cards);
-        board.cards = cards;
 
         return (
-            <div className={ className.join(" ") }>
-                <div>
-                    { cards }
+            <>
+                <div className={ className.join(" ") }>
+                    <div>{ cards }</div>
                 </div>
-            </div>
+                <div className="component-game--status">{ this.state.gameMessage }</div>
+            </>
         );
     }
 }
--- src/component/Game.js
+++ src/component/Game.js
@@ -2,43 +2,13 @@ import React from "react";
 import Board from "./Board";
 import "./Game.css";
 
-function GameStatus(props) {
-    const status = props.gameStarted ? "Click again." : "Start the game: click on a card.";
-    
-    return (
-        <div className="component-game--status">{ status }</div>
-    );
-}
-
 export default class Game extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            gameStarted: false,
-            nbPlayer: 1,
-        };
-
-        this.startGame = this.startGame.bind(this);
-    }
-
-    handleClick = () => {
-        this.setState({
-            gameStarted: true
-        });
-    }
-
-    startGame = () => { this.startGame = true }
-
     render() {
-        const className = ["component-game"];
-
         return (
-            <div className={ className.join(" ") }>
+            <div className="component-game">
                 <div className="component-game--inner" >
                     <h1>Kea Memory</h1>
-                    <Board difficultyLvl={ 8 } startGame={ this.startGame } />
-                    <GameStatus gameStarted={ this.props.gameStarted } />
+                    <Board difficultyLvl={ 2 } game="this" />
                 </div>
             </div>
         );
