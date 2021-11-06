a54f6bc4777714af585657ce599b2d59c7885e4f
Alita <dev@pinolatine.com>
2021-11-06 19:09:53 +0100

    Update style

--- src/component/Game.js
+++ src/component/Game.js
@@ -8,7 +8,7 @@ export default class Game extends React.Component {
             <div className="component-game">
                 <div className="component-game--inner" >
                     <h1>Kea Memory</h1>
-                    <Board difficultyLvl={ 2 } game="this" />
+                    <Board difficultyLvl={ 15 } game="this" />
                 </div>
             </div>
         );
